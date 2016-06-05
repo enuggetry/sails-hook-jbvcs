c2b1bb5123b4aa777d38c00258f98e0d6ab7c5dc
sakshamsaxena <ontenth@live.com>
2016-06-05 12:53:13 +0530

    Readme changes

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 This hook would primarily facilitate version control of the sequences hosted on GitHub, via the GitHub API. It aims to be flexible by providing an API which will require authenticated sessions to function.
 
-The versions (tags) of the sequence will be pulled from remote to new respective local folders, and these versions will be available for viewing on the client JBrowse. Comparison of latest local version and latest remote version can be performed, and suitable versions can be pulled accordingly. Useful status messages will be generated to elaborate on the process progress.
+The versions (tags) of the sequence will be pulled from remote to new respective local folders, and these versions will be available for viewing on the client JBrowse. Comparison of latest local version and latest remote version is performed, and suitable versions are then pulled accordingly to local. 
+
+Async nature of the hook helps in performing other stuff as well in the callbacks. 
 
 ### Quick Usage
 
@@ -11,9 +13,11 @@ var jbvcs = sails.hook.jbvcs;
 jbvcs.isApiLive(function(err, isIt) {
 	if(isIt) {
 		//API is up. We can do something now
+		//Or just say "Yipee!"
 		searchRepo("foo", "bar", function(err, foundIt) {
 			if(foundIt) {
 				//Repo exists. Do stuff now.
+				//Or just say "Woweee!"
 			}
 			else
 				throw err;
@@ -38,5 +42,11 @@ searchRepo(String username, String repository, Function callback(String err, Boo
 
 Searches for the repository on GitHub. **username** is the GitHub username, **repository** is that one which is to be used, and **callback** is a function which is returned with search results after search is complete. **foundIt** will be either true or false depending on whether repository exists or not. If it doesn't, an error message **err** is provided.
 
+It also stores the info of the latest tagged release of the same repo behind the scenes. This *might* seem a bit slow at times so...
+
 ### Testing
-Run tests with ```mocha```
+Run tests once after clone/install with ```mocha```.
+While developing, use ```npm test``` or ```mocha -w``` (same commands actually)
+
+### License
+MIT
--- index.js
+++ index.js
@@ -16,90 +16,90 @@ module.exports = function jbvcs(sails) {
 				for (var i = 0; i < data.length; i++) {
 					remoteTagList.push({});
 					remoteTagList[i].name = (data[i]).name;
-				remoteTagList[i].commit_sha = (data[i]).commit.sha;
-			remoteTagList[i].zipball_url = (data[i]).zipball_url;
-	}
-	cb();
-}
-}
-xhr.open('GET', repoInfo.tags_url, true);
-xhr.send();
-};
-
-function cloneTaggedVersions() {
-	//run clone and save to a properly named directory
-};
-
-return {
-	isApiLive: function(cb) {
-		var xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState == 4 && xhr.status == 200) {
-				var status = JSON.parse(xhr.responseText).status; //will be either "good", "minor", "major"
-				if (status === "good")
-					cb(null, true);
-				else
-					cb("API is down", false);
+					remoteTagList[i].commit_sha = (data[i]).commit.sha;
+					remoteTagList[i].zipball_url = (data[i]).zipball_url;
+				}
+				cb();
 			}
-		};
-		xhr.open('GET', 'https://status.github.com/api/status.json', true);
+		}
+		xhr.open('GET', repoInfo.tags_url, true);
 		xhr.send();
-	},
+	};
 
-	searchRepo: function(username, repository, cb) {
-		var uri = 'https://api.github.com/users/' + username + '/repos';
-		var repoExists = null;
-		//Get the list of public repos of 'username'
-		var xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState == 4 && xhr.status == 200) {
-				//Get it in JSON
-				var data = JSON.parse(xhr.responseText);
-				//iterate over each object (repo)
-				for (var i = 0; i < data.length; i++) {
-					//iterate over each key of object (repo)
-					for (var field in data[i]) {
-						//search only name in current repo/object
-						if (field === 'name') {
-							//got a match? return!
-							if (((data[i])[field]).toString() == repository) {
-								repoExists = true;
-								repoInfo.name = data[i].name;
-								repoInfo.full_name = data[i].full_name;
-								repoInfo.fork = data[i].fork;
-								repoInfo.tags_url = data[i].tags_url; //list all tags
-								repoInfo.branches_url = data[i].branches_url.substr(0, data[i].branches_url.length - 9); //list all branches
-								repoInfo.created_at = data[i].created_at;
-								repoInfo.pushed_at = data[i].pushed_at;
-								repoInfo.updated_at = data[i].updated_at;
-								fetchTags(function() {
-									cb(null, true);
-								});
-								return;
+	function cloneTaggedVersions() {
+		//run clone and save to a properly named directory
+	};
+
+	return {
+		isApiLive: function(cb) {
+			var xhr = new XMLHttpRequest();
+			xhr.onreadystatechange = function() {
+				if (xhr.readyState == 4 && xhr.status == 200) {
+					var status = JSON.parse(xhr.responseText).status; //will be either "good", "minor", "major"
+					if (status === "good")
+						cb(null, true);
+					else
+						cb("API is down", false);
+				}
+			};
+			xhr.open('GET', 'https://status.github.com/api/status.json', true);
+			xhr.send();
+		},
+
+		searchRepo: function(username, repository, cb) {
+			var uri = 'https://api.github.com/users/' + username + '/repos';
+			var repoExists = null;
+			//Get the list of public repos of 'username'
+			var xhr = new XMLHttpRequest();
+			xhr.onreadystatechange = function() {
+				if (xhr.readyState == 4 && xhr.status == 200) {
+					//Get it in JSON
+					var data = JSON.parse(xhr.responseText);
+					//iterate over each object (repo)
+					for (var i = 0; i < data.length; i++) {
+						//iterate over each key of object (repo)
+						for (var field in data[i]) {
+							//search only name in current repo/object
+							if (field === 'name') {
+								//got a match? return!
+								if (((data[i])[field]).toString() == repository) {
+									repoExists = true;
+									repoInfo.name = data[i].name;
+									repoInfo.full_name = data[i].full_name;
+									repoInfo.fork = data[i].fork;
+									repoInfo.tags_url = data[i].tags_url; //list all tags
+									repoInfo.branches_url = data[i].branches_url.substr(0, data[i].branches_url.length - 9); //list all branches
+									repoInfo.created_at = data[i].created_at;
+									repoInfo.pushed_at = data[i].pushed_at;
+									repoInfo.updated_at = data[i].updated_at;
+									fetchTags(function() {
+										cb(null, true);
+									});
+									return;
+								}
 							}
 						}
 					}
+					//case when repo isn't found
+					if (!repoExists) {
+						cb("Repository Not Found", false);
+					}
 				}
-				//case when repo isn't found
-				if (!repoExists) {
-					cb("Repository Not Found", false);
+				//case when username is wrong
+				if (xhr.status == 404) {
+					cb(xhr.statusText, false);
+					return;
 				}
-			}
-			//case when username is wrong
-			if (xhr.status == 404) {
-				cb(xhr.statusText, false);
-				return;
-			}
-		};
-		xhr.open('GET', uri, true);
-		xhr.send();
-	},
-	compareTags: function() {
-		//gets the latest tag of local repo
-		//compares with remote latest
-		//if they don't match then list all of them
-		//run clone of each tag
-		//"Cloning..."
-	}
-};
+			};
+			xhr.open('GET', uri, true);
+			xhr.send();
+		},
+		compareTags: function() {
+			//gets the latest tag of local repo
+			//compares with remote latest
+			//if they don't match then list all of them
+			//run clone of each tag
+			//"Cloning..."
+		}
+	};
 }
